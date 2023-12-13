69fda747dae4b0a8dc0eea978a1c14878a75354e
Kenson Hui <kenson.hui22@gmail.com>
2023-12-13 09:54:40 -0500

    Linting

--- client/client.py
+++ client/client.py
@@ -1,131 +1,112 @@
-import pyaudio
+"""Client for live speech to speech translation"""
+import logging
 import socket
+import time
+import threading
+from datetime import datetime, timezone
+import pyaudio
 import speech_recognition as sr
 import numpy as np
 import sounddevice as sd
-import time
-import threading
-import logging
-from datetime import datetime, timezone
 from utils.print_audio import print_sound, get_volume_norm, convert_and_normalize
 
 class AudioSocketClient:
+    """ Client for recording audio, streaming it to the server via sockets, receiving
+    the data and then piping it to an output audio device """
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 16000
     CHUNK = 4096
-    
     # Used for Speech Recognition library - set this higher for non-English languages
     PHRASE_TIME_LIMIT = 2
     # How long you need to stop speaking to be considered an entire phrase
     PAUSE_THRESHOLD = 0.5
-    
     def __init__(self) -> None:
-        self.INPUT_MICROPHONE_INDEX, self.OUTPUT_MICROPHONE_INDEX = sd.default.device
+        self.input_device_index, self.output_device_index = sd.default.device
         # TODO: Move this to a main function
         print(sd.query_devices())
-        print(f"Using input index of: {self.INPUT_MICROPHONE_INDEX}\n output index of: {self.OUTPUT_MICROPHONE_INDEX}.")
+        print(f"Using input index of: {self.input_device_index}\noutput index of: {self.output_device_index}.")
         if input(" Is this correct?\n y/[n]: ") != "y":
-            self.INPUT_MICROPHONE_INDEX = int(input("Type the index of the physical microphone: "))
-            self.OUTPUT_MICROPHONE_INDEX = int(input("Type the index of the output microphone: "))
-            
+            self.input_device_index = int(input("Type the index of the physical microphone: "))
+            self.output_device_index = int(input("Type the index of the output microphone: "))
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.recorder = sr.Recognizer()
         self.recorder.energy_threshold = 1000
-        # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
+        """Definitely do this, dynamic energy compensation lowers the energy threshold dramatically 
+            to a point where the SpeechRecognizer never stops recording."""
         self.recorder.dynamic_energy_threshold = False
-        
         self.recorder.pause_threshold = self.PAUSE_THRESHOLD
-        
         # If you're on Linux you'll need to actually list the microphone devices
         #   here I'm being lazy
-        self.source = sr.Microphone(device_index=self.INPUT_MICROPHONE_INDEX, sample_rate=self.RATE)
-        
+        self.source = sr.Microphone(device_index=self.input_device_index, sample_rate=self.RATE)
         self.transcription = [""]
-        
         ### Debugging variables
         self.time_last_sent = None
         self.time_first_received = None
         self.time_last_received = None
         self.volume_input = 0
         self.volume_output = 0
-        
         # How much time since the last received packet to refresh the flush
         self.time_flush_received = 2
-        self.debug_thread = threading.Thread(target=self.__debug_worker__, daemon=True).start()
-        
-        
+        threading.Thread(target=self.__debug_worker__, daemon=True).start()
     def __del__(self):
         # Destroy Audio resources
-        
         print('Shutting down')
 
-    # Callback function for microphone input, fires when there is new data from the microphone
     def record_callback(self, _, audio: sr.AudioData):
+        """ Callback function for microphone input, fires when there is new data from the microphone """
         data = audio.get_raw_data()
         self.time_last_sent = time.time()
         logging.debug(f"send audio data {self.time_last_sent}")
-        
         self.socket.send(data)
-        
         # convert to np array for volume
-  
         self.volume_input = get_volume_norm(
             convert_and_normalize(np.frombuffer(data, dtype=np.int16))
         )
-        
-    # Starts the event loop
     def start(self, ip, port):
-        # TODO: Seperate this out as thread based process
-        
+        """ Starts the client service """
         # Connect to server
         print(f"Attempting to connect to IP {ip}, port {port}")
         self.socket.connect((ip, port))
         print(f"Successfully connected to IP {ip}, port {port}")
-        
         with self.source:
             self.recorder.adjust_for_ambient_noise(self.source)
-        
         # Start microphone
-        self.recorder.listen_in_background(self.source, 
+        self.recorder.listen_in_background(self.source,
                                            self.record_callback,
-                                           phrase_time_limit=self.PHRASE_TIME_LIMIT)
+                                           phrase_time_limit=None)
          ## Open audio as input from microphone
-        print("Listening now...\nNote: The input microphone records in very large packets, so the volume meter won't move as much.")
-        self.volume_print_worker = threading.Thread(target=self.__volume_print_worker__, daemon=True).start()
-        with sd.OutputStream(samplerate=16000, 
-                    channels=1, 
+        print('''Listening now...\nNote: The input microphone records
+              in very large packets, so the volume meter won't move as much.''')
+        self.volume_print_worker = threading.Thread(target=self.__volume_print_worker__, daemon=True)
+        self.volume_print_worker.start()
+        with sd.OutputStream(samplerate=16000,
+                    channels=1,
                     dtype=np.float32,
-                    device=self.OUTPUT_MICROPHONE_INDEX,
+                    device=self.output_device_index,
                     ) as audio_output:
             try:
                 while True:
                     # This is where we will receive data from the server
                     packet = self.socket.recv(self.CHUNK)
-                    
                     if packet:
                         audio_chunk = np.frombuffer(packet, dtype=np.float32)
                         self.time_last_received = time.time()
-                        
                         if not self.time_first_received:
                             logging.debug(f"First audio packet - time: {self.time_last_received - self.time_last_sent}")
-                            self.time_first_received = self.time_last_received 
-                        
+                            self.time_first_received = self.time_last_received
                         # Speech T5 Output always has a sample rate of 16000
                         audio_output.write(audio_chunk)
                         self.volume_output = get_volume_norm(audio_chunk)
-                        
             except ConnectionResetError:
                 print("Server connection reset - shutting down client")
-            
             except KeyboardInterrupt:
                 print("Received keyboard input - shutting down")
-        
         # Close Socket Connection
-        self.socket.shutdown
+        self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
-
     def __volume_print_worker__(self):
+        """ Event loop for worker to continually update the terminal volume meter"""
         last_volume_input = 0
         last_volume_output = 0
         print_sound(0, 0, blocks=10)
@@ -134,7 +115,6 @@ class AudioSocketClient:
                 print_sound(self.volume_input, self.volume_output, blocks=10)
                 last_volume_input = self.volume_input
                 last_volume_output = self.volume_output
-                
             if self.time_last_sent and time.time() - self.time_last_sent > self.PHRASE_TIME_LIMIT:
                 self.volume_input = 0
             time.sleep(0.1)
@@ -142,7 +122,6 @@ class AudioSocketClient:
         """Background worker to handle debug statements"""
         print("Started background debug worker")
         while True:
-            
             if not self.time_last_sent:
                 # We can let the processor sleep more
                 time.sleep(1)
@@ -151,22 +130,17 @@ class AudioSocketClient:
                 # Data has been sent, waiting on receiving
                 time.sleep(0.05)
                 continue
-            
             if time.time() - self.time_last_received > self.time_flush_received:
                 logging.debug(f"Last audio packet - time: {self.time_last_received - self.time_last_sent}")
                 self.time_last_received = None
                 self.time_first_received = None
-        
 if __name__ == "__main__":
     date_str = datetime.now(timezone.utc)
     logging.basicConfig(filename=f"logs/{date_str}-output.log", encoding='utf-8', level=logging.DEBUG)
-    
     # Hide cursor in terminal:
     print('\033[?25l', end="")
-    
     # Start server
     client = AudioSocketClient()
     client.start('172.174.109.109', 4444)
-    
     # Show cursor again:
     print('\033[?25h', end="")
--- server/server.py
+++ server/server.py
@@ -1,73 +1,64 @@
-import json
-import pickle
-import pyaudio
-import os
-import socket
+""" Server for real-time translation and voice synthesization """
+from typing import Dict
+from queue import Queue
 import select
-import sys
+import socket
+import pyaudio
 import torch
-import threading
 from models.speech_recognition import SpeechRecognitionModel
 from models.text_to_speech import TextToSpeechModel
-from queue import Queue
-from typing import Dict
 class AudioSocketServer:
+    """ Class that handles real-time translation and voice synthesization
+        Socket input -> SpeechRecognition -> text -> TextToSpeech -> Socket output
+    """
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 44100
     CHUNK = 4096
-    
     PORT = 4444
-    # Number of unaccepted connections before server refuses new connections. 
+    # Number of unaccepted connections before server refuses new connections.
     #   For socket.listen()
-    BACKLOG = 5 
-    
+    BACKLOG = 5
     def __init__(self, whisper_model):
         self.audio = pyaudio.PyAudio()
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
         # Let kernel know we want to reuse the same port for restarting the server
         #   in quick succession
         self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
         # TODO: For multiple concurrent users we will need more queues
         #   for now we only want one user to work first
         self.data_queue : Queue = Queue()
-        
+
         # Initialize the transcriber model
         self.transcriber = SpeechRecognitionModel(model_name=whisper_model,
-                                                  data_queue=self.data_queue, 
+                                                  data_queue=self.data_queue,
                                                   generation_callback=self.handle_generation,
                                                   final_callback=self.handle_transcription)
         self.text_to_speech = TextToSpeechModel(callback_function=self.handle_synthesize)
         self.text_to_speech.load_speaker_embeddings()
         self.read_list = []
 
-
-
     def __del__(self):
-        self.data_queue.clear()
         self.audio.terminate()
         self.transcriber.stop()
         self.serversocket.shutdown()
         self.serversocket.close()
-        
     def handle_generation(self, packet: Dict):
-        pass
-        
+        """ Placeholder function for transcription"""
     def handle_transcription(self, packet: str, client_socket):
+        """ Callback function to put finalized transcriptions into TTS"""
         print(f"Added {packet} to synthesize task queue")
-        # client_socket = self.read_list[1]
         self.text_to_speech.synthesise(packet, client_socket)
-    
     def handle_synthesize(self, audio: torch.Tensor, client_socket):
+        """ Callback function to stream audio back to the client"""
         self.stream_numpy_array_audio(audio, client_socket)
 
     def start(self):
+        """ Starts the server"""
         self.transcriber.start(16000, 2)
         print(f"Listening on port {self.PORT}")
         self.serversocket.bind(('', self.PORT))
         self.serversocket.listen(self.BACKLOG)
-        
         # Contains all of the socket connections, the first is the server socket for listening to
         #   new connections. All other ones are for individual clients sending data.
         self.read_list = [self.serversocket]
@@ -85,33 +76,31 @@ class AudioSocketServer:
                             data = s.recv(4096)
 
                             if data:
-                                self.data_queue.put((s, data)) 
+                                self.data_queue.put((s, data))
                             else:
                                 self.read_list.remove(s)
                                 print("Disconnection from", address)
-                        except ConnectionResetError as e:
+                        except ConnectionResetError:
                             self.read_list.remove(s)
                             print("Client crashed from", address)
-                            
         except KeyboardInterrupt:
             pass
-        
         print("Performing server cleanup")
         self.audio.terminate()
         self.transcriber.stop()
         self.serversocket.shutdown(socket.SHUT_RDWR)
         self.serversocket.close()
         print("Sockets cleaned up")
-        
     def stream_numpy_array_audio(self, audio, client_socket):
+        """ Streams audio back to the client"""
         try:
             client_socket.sendall(audio.numpy().tobytes())
-        except Exception as e:
+        except ConnectionResetError as e:
             print(f"Error sending audio to client: {e}")
             if client_socket in self.read_list:
                 self.read_list.remove(client_socket)
 
-
 if __name__ == "__main__":
     server = AudioSocketServer(whisper_model="base")
     server.start()
+    
